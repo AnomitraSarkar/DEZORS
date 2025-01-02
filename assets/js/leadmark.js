e74dc0ab1825a1782ab0489396bf46a5c75c78b3
Anomitra Sarkar <anomitrasarkar05@gmail.com>
2025-01-02 13:35:09 +0530

    Loader changes

--- assets/js/leadmark.js
+++ assets/js/leadmark.js
@@ -31,6 +31,25 @@ $(document).ready(function(){
     });
 });
 
+
+$.get("../../loader.html", function(data){
+    $("#loading").replaceWith(data);
+  });
+  
+
+  $(window).on('load', function(){
+    setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+  });
+  function removeLoader(){
+      $( ".loader-wrapper" ).fadeOut(500, function() {
+        // fadeOut complete. Remove the loading div
+        $( ".loader-wrapper" ).remove(); //makes page more lightweight 
+    });   
+  }
+  
+
+
+
 // protfolio filters
 $(window).on("load", function() {
     var t = $(".portfolio-container");
