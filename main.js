076c8f51d571a1621c6196bbb1f004b773e55864
AnomitraSarkar <anomitrasarkar05@gmail.com>
2025-01-10 22:11:42 +0530

    Cleared code up

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
 console.log("Start the backend");
 
-// // $(window).on("load", function () {
-// //   console.log("Loader was triggered");
-//   setTimeout(removeLoader, 1500); //wait for page load PLUS two seconds.
-// // });
-// function removeLoader() {
-//   $(".loader-wrapper").fadeOut(500, function () {
-//     // fadeOut complete. Remove the loading div
-//     $(".loader-wrapper").remove(); //makes page more lightweight
-//   });
-// }
-
 console.log("End the backend");
